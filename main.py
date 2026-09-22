9147cfa3fd9d3b0eaf9254a5edfa01fbe4feb5d8
Amber088 <amberjain967@gmail.com>
2026-09-22 15:00:53 +0530

    Deployed

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
+@app.post("/users")
+async def create_user(name: str):
+    return {"message": "User created", "name": name}
 
 @app.get("/about")
 async def about():
